c5a26723c8941ffe1a440e2eb35c9c4e2178a14c
Amodhx <amxdhnanditha@gmail.com>
2025-02-09 22:25:59 +0530

    created tasks table

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,23 +1,115 @@
-import {theme} from "antd";
 import {Content} from "antd/es/layout/layout";
+import {Flex, Table, Tag} from 'antd';
+import type { TableProps } from 'antd';
+import * as React from "react";
 
+interface DataType {
+    key: string;
+    task: string;
+    status: string;
+    tags: string[];
+    dueDate: string;
+    estimatedTime: string;
+}
+const columns: TableProps<DataType>['columns'] = [
+    {
+        dataIndex: 'task',
+        key: 'task',
+    },
+    {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+    },
+    {
+        title: 'Tags',
+        key: 'tags',
+        dataIndex: 'tags',
+        render: (_, { tags }) => (
+            <>
+                {tags.map((tag) => {
+                    let color = tag.length > 5 ? 'geekblue' : 'green';
+                    if (tag === 'New Tasks') {
+                        color = 'volcano';
+                    }else if (tag === 'In Progress') {
+                        color = 'geekblue'
+                    }else if (tag === 'Completed') {
+                        color = 'green';
+
+                    }
+                    return (
+                        <Tag color={color} key={tag}>
+                            {tag.toUpperCase()}
+                        </Tag>
+                    );
+                })}
+            </>
+        ),
+    },
+    {
+        title: 'Due Date',
+        dataIndex: 'dueDate',
+        key: 'dueDate',
+    },
+    {
+        title: 'Estimated Time',
+        key: 'estimatedTime',
+        dataIndex: 'estimatedTime',
+    }
+
+];
+const data: DataType[] = [
+    {
+        key: '1',
+        task: 'lorem sadnojsadn sajdnas dasdsa sad sadsa dsadsad sa dsad sa dsa as',
+        status: 'In progress',
+        tags: ['In progress'],
+        dueDate : '2025/02/12',
+        estimatedTime : '25h'
+    },
+    {
+        key: '1',
+        task: 'lorem sadnojsadn sajdnas dasdsa sad sadsa dsadsad sa dsad sa dsa as',
+        status: 'Completed',
+        tags: ['Completed'],
+        dueDate : '2025/02/12',
+        estimatedTime : '25h'
+    },
+    {
+        key: '1',
+        task: 'lorem sadnojsadn sajdnas dasdsa sad sadsa dsadsad sa dsad sa dsa as',
+        status: 'New Tasks',
+        tags: ['New Tasks'],
+        dueDate : '2025/02/12',
+        estimatedTime : '25h'
+    },
+];
+const tagsData = ['All','New Tasks','In Progress','Completed'];
 function Tasks(){
-    const {
-        token: { colorBgContainer, borderRadiusLG },
-    } = theme.useToken();
+    const [selectedTags, setSelectedTags] = React.useState<string>('All');
+    const handleChange = (tag: string) => {
+        const nextSelectedTags = tag
+        console.log('You are interested in: ', nextSelectedTags);
+        setSelectedTags(nextSelectedTags);
+    };
     return(
         <>
             <Content>
-                <div
-                    style={{
-                        padding: 24,
-                        minHeight: 360,
-                        background: colorBgContainer,
-                        borderRadius: borderRadiusLG,
-                        margin : '10px'
-                    }}
-                >
-                    Tasks
+                <div className={'mx-4 mt-2'}>
+                    <Flex gap={4} wrap align="center">
+                        {tagsData.map<React.ReactNode>((tag) => (
+                            <Tag.CheckableTag
+                                key={tag}
+                                checked={selectedTags.includes(tag)}
+                                onChange={() => handleChange(tag)}
+                            >
+                                {tag}
+                            </Tag.CheckableTag>
+                        ))}
+                    </Flex>
+                </div>
+                <div className={'mx-2 mt-2'}>
+                    <Table<DataType> columns={columns} dataSource={data} />
                 </div>
             </Content>
         </>
